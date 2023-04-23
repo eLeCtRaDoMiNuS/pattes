e561bb871198fdcca3c49adbb2756b11123c96a2
npm <77410796+eLeCtRaDoMiNuS@users.noreply.github.com>
2023-04-23 12:12:08 +0300

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 13076380114 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/eLeCtRaDoMiNuS/pattes/main/hero.lua",true))()
+elseif game.PlaceId == 10449761463 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/eLeCtRaDoMiNuS/pattes/main/saitama.lua",true))()
+end
